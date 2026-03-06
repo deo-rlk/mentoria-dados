3b561805940d16aaf01f9e630d9b26323f8b3e40
Odewaldo <odewaldorodrigues@gmail.com>
2026-03-05 22:47:57 -0300

    Commit do primeiro desafio

--- desafio_01/main.py
+++ desafio_01/main.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 url = 'https://raw.githubusercontent.com/Moscarde/Junior_Zone/automation/data/googlesheets_dataset.csv'
 
+# local_data = 'C:/Users/odewaldo.rodrigues_i/Desktop/GitHub/mentoria-dados/Junior_Zone/data/googlesheets_dataset.csv'
+
 data = pd.read_csv(url, sep=',')
+
 dataFrame = pd.DataFrame(data)
 
 path = 'C:/Users/odewaldo.rodrigues_i/Desktop/GitHub/mentoria-dados/dados/vagas/data.json'
